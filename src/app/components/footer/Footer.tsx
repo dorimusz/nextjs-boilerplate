35c5e4dddd9ba6093b2160b411652c5a2a1f1220
dorimusz <78429829+dorimusz@users.noreply.github.com>
2024-11-12 23:19:25 +0100

    add socket to footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './footer.module.css';
 import MiddleColumn from './MiddleColumn';
 import SocialIcons from './SocialIcons';
+import Socket from './Socket';
 import { LabeledLinkType } from '@/app/types/global';
 
 const companyLogo =
@@ -16,35 +17,40 @@ const socialLinks: LabeledLinkType[] = [
 ];
 const Footer: React.FC = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.columnContainer}>
-        <div className={styles.leftColumn}>
-          <h2 className={styles.footerTitle}>This a catchy line to engage!</h2>
-          <p className={styles.footerText}>
-            Iusto corporis harum odit provident cupiditate, consequatur mollitia
-            sequi veritatis perferendis dolore repellendus repellat error
-            architecto doloremque aliquid blanditiis culpa quo facere.
-          </p>
-          <div className={styles.buttonContainer}>
-            <button className={styles.footerButton}>Click Me</button>{' '}
+    <footer className={styles.container}>
+      <div className={styles.footer}>
+        <div className={styles.columnContainer}>
+          <div className={styles.leftColumn}>
+            <h2 className={styles.footerTitle}>
+              This a catchy line to engage!
+            </h2>
+            <p className={styles.footerText}>
+              Iusto corporis harum odit provident cupiditate, consequatur
+              mollitia sequi veritatis perferendis dolore repellendus repellat
+              error architecto doloremque aliquid blanditiis culpa quo facere.
+            </p>
+            <div className={styles.buttonContainer}>
+              <button className={styles.footerButton}>Click Me</button>{' '}
+            </div>
+          </div>
+
+          <div className={styles.middleColumn}>
+            <MiddleColumn
+              companyLogo={companyLogo}
+              altText={'Comapny name logo'}
+            />
+          </div>
+
+          <div className={styles.rightColumn}>
+            <h2 className={styles.footerTitle}>Social connection</h2>
+            <p className={styles.footerText}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <SocialIcons socialLinks={socialLinks} />
           </div>
         </div>
-
-        <div className={styles.middleColumn}>
-          <MiddleColumn
-            companyLogo={companyLogo}
-            altText={'Comapny name logo'}
-          />
-        </div>
-
-        <div className={styles.rightColumn}>
-          <h2 className={styles.footerTitle}>Social connection</h2>
-          <p className={styles.footerText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </p>
-          <SocialIcons socialLinks={socialLinks} />
-        </div>
       </div>
+      <Socket />
     </footer>
   );
 };
---
+++ src/app/components/footer/Socket.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+const Socket: React.FC = () => {
+  return (
+    <div className={styles.socket}>
+      <p>© 2024 drms</p>
+    </div>
+  );
+};
+
+export default Socket;
