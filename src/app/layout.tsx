723425f47b75c28fb91d0089d7c259ab66038da5
dorimusz <78429829+dorimusz@users.noreply.github.com>
2024-11-18 12:26:25 +0100

    add font usages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import NavBar from './components/navigation/NavBar';
 import Footer from './components/footer/Footer';
 import dynamic from 'next/dynamic';
 import { cookies } from 'next/headers';
-// import { MonaSans_Font } from '@/assets/fonts/MonaSans';
 
-// Dynamic Import: The dynamic function is used to import the AppThemeProvider component dynamically. If you want to disable pre-rendering for a Client Component, you can use the ssr option set to false
-// SSR Disabled: The ssr: false option ensures that the AppThemeProvider component is only rendered on the client side, not on the server. This is useful for components that rely on client-side state or browser-specific APIs.
-const AppThemeProvider = dynamic(() => import('./providers/theme'), {
-  ssr: false,
-});
-// Implementing a Theme Provider in Next.js 14+ without wrapping children in Context offers a cleaner and more efficient way to manage themes in your application without making complete application a client component
+/*
+//Importing Google Fonts.
+ import { Oooh_Baby } from 'next/font/google';
+ const ooohBabyFont = Oooh_Baby({ subsets: ['latin'], weight: ['400'] });
+
+// usage: <body className={ooohBaby.className}>
+*/
+
+/*
+//Importing local Fonts.
+import localFont from 'next/font/local';
+import { MonaSans_Font } from '@/assets/fonts/MonaSans';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -24,6 +28,18 @@ const geistMono = localFont({
   variable: '--font-geist-mono',
   weight: '100 900',
 });
+
+// usage: 
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+         ${MonaSans_Font.className}>
+*/
+
+// Dynamic Import: The dynamic function is used to import the AppThemeProvider component dynamically. If you want to disable pre-rendering for a Client Component, you can use the ssr option set to false
+// SSR Disabled: The ssr: false option ensures that the AppThemeProvider component is only rendered on the client side, not on the server. This is useful for components that rely on client-side state or browser-specific APIs.
+const AppThemeProvider = dynamic(() => import('./providers/theme'), {
+  ssr: false,
+});
+// Implementing a Theme Provider in Next.js 14+ without wrapping children in Context offers a cleaner and more efficient way to manage themes in your application without making complete application a client component
 
 export const metadata: Metadata = {
   title: 'Add site title here',
@@ -46,8 +62,7 @@ export default function RootLayout({
       className={theme}
       style={theme !== 'system' ? { colorScheme: theme } : {}}
     >
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {/* ${MonaSans_Font.className} */}
+      <body>
         <AppThemeProvider attribute="class" defaultTheme={theme} enableSystem />
         <NavBar />
         {children}
