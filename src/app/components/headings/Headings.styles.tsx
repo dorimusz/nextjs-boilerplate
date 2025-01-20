c77404274170e3dd578ea941511f44df5d459d4e
dorimusz <78429829+dorimusz@users.noreply.github.com>
2025-01-20 09:15:09 +0100

    add styled-components headings

---
+++ src/app/components/headings/Headings.styles.tsx
@@ -0,0 +1,22 @@
+'use client';
+import styled from 'styled-components';
+
+const MainHeading = styled.h1`
+  color: palevioletred;
+`;
+
+const SubheadingH2 = styled.h2`
+  color: rgb(143, 112, 216);
+  text-align: center;
+  padding: 0.4rem 0;
+`;
+
+const SubheadingH3 = styled.h3`
+  color: rgb(112, 18, 50);
+`;
+
+const SubheadingH4 = styled.h4`
+  color: rgb(15, 119, 55);
+`;
+
+export { MainHeading, SubheadingH2, SubheadingH3, SubheadingH4 };
